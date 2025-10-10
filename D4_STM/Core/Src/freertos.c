cdc9e8f890327f3a68f859606c7a4217c7efb84f
remi <remplante@hotmail.com>
2025-10-10 14:20:18 -0400

    Code controle servo

--- D4_STM/Core/Src/freertos.c
+++ D4_STM/Core/Src/freertos.c
@@ -22,6 +22,7 @@
 #include "task.h"
 #include "main.h"
 #include "cmsis_os.h"
+#include "Servo/Servo.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -64,6 +65,8 @@ const osThreadAttr_t xCLI_attributes = {
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
+
+
 
 /* USER CODE END FunctionPrototypes */
 
@@ -125,10 +128,23 @@ void MX_FREERTOS_Init(void) {
 void StartDefaultTask(void *argument)
 {
   /* USER CODE BEGIN StartDefaultTask */
+
   /* Infinite loop */
   for(;;)
   {
-    osDelay(1);
+	  //
+	  setServoDuty(1, 0.15);
+	  setServoDuty(2, 0.15);
+	  setServoDuty(3, 0.15);
+	  //AppTime_TaskDelayMs(1000)
+	  osDelay(1000);
+	  setServoDuty(1, 0.53);
+	  setServoDuty(2, 0.53);
+	  setServoDuty(3, 0.53);
+	  osDelay(1000);
+	  //TIM4->CCR1 = 8000;
+	  //AppTime_TaskDelayMs(1000)
+	  //osDelay(500);
   }
   /* USER CODE END StartDefaultTask */
 }
--- D4_STM/Core/Src/main.c
+++ D4_STM/Core/Src/main.c
@@ -100,7 +100,10 @@ int main(void)
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_4);
   /* USER CODE END 2 */
 
   /* Init scheduler */
